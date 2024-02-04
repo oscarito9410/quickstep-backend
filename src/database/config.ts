2bbb98219743c6444ddd65ff29fe8d9d773f1983
OSCAR EMILIO PÉREZ MARTÍNEZ <13366923+oscarito9410@users.noreply.github.com>
2024-02-04 13:21:37 -0600

    Update config.ts

--- src/database/config.ts
+++ src/database/config.ts
@@ -6,11 +6,10 @@ dotenv.config();
 
 const DB_URL = process.env.MONGO_DB_URL!;
 
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 const hardcodeUri = "mongodb+srv://oscar:66c51SF60spi9gf6@cluster0.wbsmp4b.mongodb.net/?retryWrites=true&w=majority";
 export async function connectDB() {
   try {
-    await mongoose.connect(hardcodeUri, clientOptions);
+    await mongoose.connect(hardcodeUri);
     await mongoose.connection.db.admin().command({ ping: 1 });
     console.log(`MongoDB connected ⭐️`);
   } catch (error) {
